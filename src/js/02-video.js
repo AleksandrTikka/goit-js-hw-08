5beec106f20fde2abc33b4c6cec84bb630c60e58
AleksandrTikka <alexandertikka@gmail.com>
2022-06-11 00:06:37 +0300

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import trottle from 'lodash.throttle';
+
+    const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+    
+const key = 'videoplayer-current-time';
+const savedTime = localStorage.getItem(key);
+
+
+    const onPlay = function(e) {
+    // data is an object containing properties specific to that event
+        
+        
+        localStorage.setItem(key, e.seconds);
+};
+
+if (savedTime) { player.setCurrentTime(savedTime) };
+
+player.on('timeupdate', trottle(onPlay, 1000));
+
+
 
