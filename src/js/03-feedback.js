853debaee4889be59505f83dee7fc55aeb97fb46
AleksandrTikka <alexandertikka@gmail.com>
2022-06-12 01:28:50 +0300

    task 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,33 +1,50 @@
 import throttle from "lodash.throttle";
 const refs = {
     form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form input'),
     textarea: document.querySelector('.feedback-form textarea'),
 }
 
-const user = {
-//     email,
-//     message,
-}
-// const dataBase = localStorage.setItem("feedback-form-state", JSON.stringify(user))
+const user = {};
+const KEY = "feedback-form-state";
 refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', throttle(onEmailInput, 500));
+// refs.form.addEventListener('input', throttle(onInput, 500));
+refs.input.addEventListener('input', throttle(onEmailInput, 500));
 refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+populateText();
 
 function onFormSubmit(e) {
     e.preventDefault();
-    e.currentTarget.reset()
+    console.log(user);
+    e.currentTarget.reset();
+    localStorage.removeItem(KEY);
 };
 
 function onEmailInput(e) {
     user.email = e.target.value;
-    console.log(user.email);
-    localStorage.setItem("feedback-form-state", JSON.stringify(user))
+    console.log("значение поля email:", user.email);
+    localStorage.setItem(KEY, JSON.stringify(user))
 }
 
 function onTextareaInput(e) {
-    // user.message = e.target.value;
+    user.message = e.target.value;
+    localStorage.setItem(KEY, JSON.stringify(user));
+    console.log("значение поля message:", user.message);
+}
 
-    const user.emailvalue = e.target.value;
-    localStorage.setItem("feedback-form-state",value);
-    console.log(value);
+// function onInput(evt) {
+//     user[evt.target.name] = evt.target.value;
+//     localStorage.setItem(KEY, JSON.stringify(user));
+//     console.log(user);
+// }
+
+function populateText() {
+    const savedObj = (localStorage.getItem(KEY));
+    if (savedObj) {
+        console.log(savedObj);
+        refs.input.value = JSON.parse(savedObj).email;
+        refs.textarea.value = JSON.parse(savedObj).message;
+        
+    }
 }
